e50ab6f04462c1c02d96690854fc4d3abbccf86f
belalElshafei <71639202+belalElshafei@users.noreply.github.com>
2021-07-17 21:14:24 +0200

    Problem Solution

---
+++ George the dictator.cpp
@@ -0,0 +1,33 @@
+//Problem link :=> https://codeforces.com/group/uIwyWI1OoG/contest/286425/problem/P
+
+//By Belal Elshafei
+
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+	int	n,k;
+	cin>>n>>k;
+	vector<int>v(n);
+	map<int,int> mp;
+	 
+	for(int i = 0; i < n; i++){
+		cin>>v[i];
+		mp[v[i]]++;
+	}
+	
+	ll res = 0;
+	for(int i = 0; i < n; i++){
+		
+		int need = k - v[i];
+		
+		if(mp[need] > 0){
+			mp[v[i]]--;
+			res+=mp[need];
+		}
+	}
+		
+	cout<<res<<"\n";
+}
